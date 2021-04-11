c57d76f7707f9712aa86015c1a533c7e46217e96
Joseph Abell <jsphabll@gmail.com>
2021-04-11 20:51:30 +0100

    add node modules

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,16 +1,12 @@
-const https = require('https');
+const axios = require('axios');
 
 exports.handler = async (event, context) => {
     console.info('Deploy Succeeded')
     const body = JSON.parse(event.body);
     const commitRef = body.payload.commit_ref;
     console.info('Commit Ref:', commitRef)
-    https.get(`https://api.github.com/repos/joseph-abell/website-data/commits/${commitRef}`, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-
-        res.on('data', (d) => {
-            console.log(d);
-        });
-    });
+    const url = `https://api.github.com/repos/joseph-abell/website-data/commits/${commitRef}`;
+    const data = await axios(url)
+    console.log(url)
+    console.log(data);
 }
