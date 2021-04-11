18d396790ee2bb6f0f7509339342b9c0181e233b
Joseph Abell <jsphabll@gmail.com>
2021-04-11 20:38:04 +0100

    succeeded

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,7 +1,16 @@
+import https from 'https';
+
 exports.handler = async (event, context) => {
     console.info('Deploy Succeeded')
     const body = JSON.parse(event.body);
     const commitRef = body.payload.commit_ref;
-    console.log('Commit Ref:', commitRef)
-    console.log(process.env.GITHUB_API_KEY);
+    console.info('Commit Ref:', commitRef)
+    https.get(`https://api.github.com/repos/joseph-abell/website-data/commits/${commitRef}`, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            console.log(d);
+        });
+    });
 }
