7a32847abaec1374601e73ff7b73f1dce1c0c520
Joseph Abell <jsphabll@gmail.com>
2021-04-11 20:08:17 +0100

    commit ref

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,8 +1,7 @@
 exports.handler = async (event, context) => {
     console.info('Deploy Succeeded')
     const body = JSON.parse(event.body);
-    const commitRef = body.payload.commitRef;
+    const commitRef = body.payload.commit_ref;
     console.log('Commit Ref:', commitRef)
-    console.log(body.payload);
     console.log(process.env.GITHUB_API_KEY);
 }
